68fe5f2b0db52957e38e72047d11262bb1146812
Neramit <DavidTh30@gmail.com>
2026-07-30 21:39:05 +0700

    Add files via upload

---
+++ Shared Memory03/FileMappingDebug/unit1.pas
@@ -0,0 +1,182 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+//HANDLE CreateFileMappingA(
+//  [in]           HANDLE                hFile,
+//  [in, optional] LPSECURITY_ATTRIBUTES lpFileMappingAttributes,
+//  [in]           DWORD                 flProtect,
+//  [in]           DWORD                 dwMaximumSizeHigh,
+//  [in]           DWORD                 dwMaximumSizeLow,
+//  [in, optional] LPCSTR                lpName
+//);
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
+  Windows, Unit2;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Memo1: TMemo;
+    Memo2: TMemo;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    TabSheet3: TTabSheet;
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+  private
+
+  public
+    procedure OnIdle(Sender: TObject; var Done: boolean);
+    procedure OnIdleEnd(Sender: TObject);
+    constructor Create(TheOwner: TComponent);  override;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.OnIdle(Sender: TObject; var Done: boolean);
+begin
+
+  if pBuf <> nil then
+  begin
+    if String(pBuf) <> '' then
+    begin
+      _tprintf(String(pBuf));
+      StrCopy(pBuf, '');
+    end;
+  end;
+
+  if pBuf2 <> nil then
+  begin
+    if String(pBuf2) <> '' then
+    begin
+      _tprintf2(String(pBuf2));
+      StrCopy(pBuf2, '');
+    end;
+  end;
+
+  Done := false;
+end;
+
+procedure TForm1.OnIdleEnd(Sender: TObject);
+begin
+  // OnIdleEnd run when OnIdle  Done := true;
+end;
+
+constructor TForm1.Create(TheOwner: TComponent);
+begin
+inherited Create(TheOwner);
+  Memo_:=Memo1;
+  Memo__:=Memo2;
+  Application.OnIdle := @OnIdle;
+  Application.OnIdleEnd:=@OnIdleEnd;
+
+  SetLastError(0);
+
+   hMapFile := CreateFileMapping(
+    INVALID_HANDLE_VALUE, // Use paging file instead of an actual file
+    nil,                  // Default security
+    PAGE_READWRITE,       // Read/write access
+    0,                    // Maximum object size (high-order DWORD)
+    MEMORY_SIZE,          // Maximum object size (low-order DWORD)
+    CMapName              // Name of mapping object
+  );
+
+  if hMapFile = 0 then
+  begin
+    _tprintf('Could not create file mapping object. Error: '+ SysErrorMessage(GetLastError));
+    halt;
+  end;
+
+   hMapFile2 := CreateFileMapping(
+    INVALID_HANDLE_VALUE, // Use paging file instead of an actual file
+    nil,                  // Default security
+    PAGE_READWRITE,       // Read/write access
+    0,                    // Maximum object size (high-order DWORD)
+    MEMORY_SIZE,          // Maximum object size (low-order DWORD)
+    CMapName2              // Name of mapping object
+  );
+
+  if hMapFile2 = 0 then
+  begin
+    _tprintf2('Could not create file mapping object. Error: '+ SysErrorMessage(GetLastError));
+    halt;
+  end;
+
+  pBuf := PChar(MapViewOfFile(
+    hMapFile,            // Handle to map object
+    FILE_MAP_ALL_ACCESS, // Read/write permission
+    0,
+    0,
+    MEMORY_SIZE
+  ));
+
+  if pBuf = nil then
+  begin
+    _tprintf('Could not map view of file. Error: '+ SysErrorMessage(GetLastError));
+    CloseHandle(hMapFile);
+    halt;
+  end;
+
+  pBuf2 := PChar(MapViewOfFile(
+    hMapFile2,            // Handle to map object
+    FILE_MAP_ALL_ACCESS, // Read/write permission
+    0,
+    0,
+    MEMORY_SIZE
+  ));
+
+  if pBuf2 = nil then
+  begin
+    _tprintf2('Could not map view of file. Error: '+ SysErrorMessage(GetLastError));
+    CloseHandle(hMapFile2);
+    halt;
+  end;
+
+  StrCopy(pBuf, '');
+  StrCopy(pBuf2, '');
+
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  if pBuf <> nil then
+  begin
+    UnmapViewOfFile(pBuf);
+  end;
+  if hMapFile <> 0 then
+  begin
+    CloseHandle(hMapFile);
+    hMapFile := 0;
+  end;
+
+  if pBuf2 <> nil then
+  begin
+    UnmapViewOfFile(pBuf2);
+  end;
+  if hMapFile2 <> 0 then
+  begin
+    CloseHandle(hMapFile2);
+    hMapFile2 := 0;
+  end;
+
+  //_tprintf('Execution completed successfully.');
+
+end;
+
+end.
+
---
+++ Shared Memory03/FileMappingDebug/unit2.pas
@@ -0,0 +1,77 @@
+unit Unit2;
+
+{$mode ObjFPC}{$H+}
+{$LONGSTRINGS ON}
+
+interface
+
+uses
+  Classes, SysUtils, StdCtrls, Windows;
+
+procedure _tprintf(s:string);
+procedure _tprintf2(s:string);
+function RandomString(Size: Integer): String;
+
+type
+  //{$ScopedEnum On}
+  TChars = (
+    a, c, g, t
+  );
+
+  ch_array = array[TChars] of 1..4;
+
+const
+  MEMORY_SIZE = 1024; // 1 KB
+  CMapName = 'Global//StringDebug01';
+  CMapName2 = 'Global//StringDebug02';
+
+var
+  Memo_: TMemo;
+  Memo__: TMemo;
+  hMapFile: THandle;
+  pBuf: PChar;
+  hMapFile2: THandle;
+  pBuf2: PChar;
+
+implementation
+
+procedure _tprintf(s:string);
+begin
+  Memo_.Append(s);
+  if ((Upcase(s)='CLEAR') or (Upcase(s)='CLEAN')) then
+  begin
+    Memo_.Clear;
+  end;
+  if Memo_.Lines.Count > 20 then Memo_.Lines.Delete(0);
+end;
+
+procedure _tprintf2(s:string);
+begin
+  Memo__.Append(s);
+  if ((Upcase(s)='CLEAR') or (Upcase(s)='CLEAN')) then
+  begin
+    Memo__.Clear;
+  end;
+  if Memo__.Lines.Count > 20 then Memo__.Lines.Delete(0);
+end;
+
+function RandomString(Size: Integer): String;
+const
+  // The pool of allowed characters
+  Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var
+  i, Len: Integer;
+begin
+  Result := '';
+  Len := Length(Chars);
+  SetLength(Result, Size); // Pre-allocate string memory for performance
+
+  for i := 1 to Size do
+  begin
+    // Random(Len) returns 0 to Len-1. Pascal strings are 1-indexed.
+    Result[i] := Chars[Random(Len) + 1];
+  end;
+end;
+
+end.
+
